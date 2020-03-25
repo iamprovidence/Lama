21ce32357de4e9fa32e3ea44bf292843decc165f
iamprovidence <orleon.nilkford@gmail.com>
2020-03-25 20:26:37 +0200

    Add redis cache

--- src/backend/Aggregator/Aggregator/Startup.cs
+++ src/backend/Aggregator/Aggregator/Startup.cs
@@ -8,35 +8,37 @@ using Aggregator.ServicesConfiguration;
 
 namespace Aggregator
 {
-    public class Startup
-    {
-        public IConfiguration Configuration { get; }
+	public class Startup
+	{
+		public IConfiguration Configuration { get; }
 
-        public Startup(IHostingEnvironment hostingEnvironment)
-        {
-            Configuration = EnvironmentConfiguration.BuildConfiguration(hostingEnvironment);
-        }
+		public Startup(IHostingEnvironment hostingEnvironment)
+		{
+			Configuration = EnvironmentConfiguration.BuildConfiguration(hostingEnvironment);
+		}
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddMapper(Configuration);
-            services.AddAuthentication(Configuration);
-            services.AddSwagger(Configuration);
-            services.AddCORS(Configuration);
-            services.AddHttpServices(Configuration);
-            services.AddAggregators(Configuration);
-        }
+			services.AddMapper(Configuration);
+			services.AddAuthentication(Configuration);
+			services.AddSwagger(Configuration);
+			services.AddCORS(Configuration);
+			services.AddHttpServices(Configuration);
+			services.AddAggregators(Configuration);
+			services.AddCache(Configuration);
+			services.RegisterCacheServices(Configuration);
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            app.UseAuthentication();
-            app.UseCORS();
-            app.UseSwagger();
-            app.UseMvc();
-        }
-    }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			app.UseAuthentication();
+			app.UseCORS();
+			app.UseSwagger();
+			app.UseMvc();
+		}
+	}
 }
